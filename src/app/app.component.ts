5578a0b641507a9e6f03103f3b53801a9be29ff9
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2024-05-09 14:43:12 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var paypal: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'paypal-projectv15';
+
+
+export class AppComponent implements OnInit {
+  
+  @ViewChild('paypal', {static: true}) paypalElement!: ElementRef; 
+
+  producto = {
+    descripcion : 'crema antiarrugas',
+    precio : 20,
+    img : 'imagen del producto'
+  }
+  title = 'paypal-project';
+
+  ngOnInit(){
+    paypal
+    .Buttons({
+      createOrder: (data: any, actions: any) => { 
+        return actions.order.create({
+          purchase_units: [
+            {
+              description: this.producto.descripcion,
+              amount :{
+                currency_code: 'EUR',
+                value: this.producto.precio
+              }
+            }
+          ]
+        })
+      },
+      onApprove: async (data: any, actions: any) => { 
+        const order = await actions.order.capture();
+        console.log(order);
+      },
+      onError: (err: any) => { 
+        console.log(err);
+      },
+      fundingSource: paypal.FUNDING.PAYPAL, // Solo mostrar PayPal
+      style: {
+        layout: 'vertical', // Mostrar en un diseño vertical
+        size: 'small' // Tamaño pequeño
+      }
+    })
+    .render(this.paypalElement.nativeElement);
+  }
 }
